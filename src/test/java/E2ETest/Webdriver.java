d05b6b1df45b9cecb4a399088cc2c2f70e1a120a
Abhishek Kumar Jha <abhishekkumarjha@Abhisheks-MacBook-Pro.local>
2020-06-03 21:34:04 +0530

    New commit

--- src/test/java/E2ETest/Webdriver.java
+++ src/test/java/E2ETest/Webdriver.java
@@ -1,6 +1,14 @@
 package E2ETest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Webdriver {
     private static Webdriver instance = null;
 
